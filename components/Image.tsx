facb68b0840e70fa55bd3e3e2f8a126c73f72af7
woodi <woodi.daily@gmail.com>
2021-12-30 06:40:27 +0900

    Modify Logo & Metadata

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,19 @@
-import NextImage, { ImageProps } from 'next/image'
+// import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+// const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+
+// export default Image
+
+// import NextImage, { ImageProps } from 'next/image'
+
+interface IImageProps {
+  alt: string
+  src: string
+  className?: string
+  width?: number | string
+  height?: number | string
+}
+
+const Image = ({ ...rests }: IImageProps) => <img {...rests} />
 
 export default Image
