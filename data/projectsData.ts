f894142a436d38fe92155a4f9f5f83e27066fa5f
woodi <woodi.daily@gmail.com>
2022-02-05 00:44:26 +0900

    Write How to make advent calendar Post

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,15 @@
 const projectsData = [
   {
     title: 'Surrounds',
-    description: 'A simple, yet powerful, Voice Chat SNS Based on GPS',
+    description: 'Voice Chat SNS Based on User Location',
     imgSrc: 'static/images/proj/surrounds.png',
     href: 'https://surrounds.herokuapp.com',
+  },
+  {
+    title: 'Zeropage Advent Calendar',
+    description: 'Calendar exposures user seminar day by day',
+    imgSrc: 'static/images/proj/advent/advent.png',
+    href: 'https://woodi97.github.io/zp-advent-calendar/',
   },
 ]
 
