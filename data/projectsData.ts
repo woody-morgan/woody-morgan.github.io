59eb610dfb54277fae06fb95a8f6495f5f125c8d
woodi <woodi.daily@gmail.com>
2022-03-01 09:56:02 +0900

    Modify Project Data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,15 +1,21 @@
 const projectsData = [
   {
     title: 'Surrounds',
-    description: 'Voice Chat SNS Based on User Location',
+    description: 'Voice Chat SNS Based on Your Location',
     imgSrc: 'static/images/proj/surrounds.png',
-    href: 'https://surrounds.herokuapp.com',
+    href: 'https://surrounds.vercel.app',
   },
   {
     title: 'Zeropage Advent Calendar',
-    description: 'Calendar exposures user seminar day by day',
+    description: 'Calendar for Advent Calendar',
     imgSrc: 'static/images/proj/advent/advent.png',
     href: 'https://woodi97.github.io/zp-advent-calendar/',
+  },
+  {
+    title: 'Nextjs Boilerplate Project',
+    description: 'Nextjs Boilerplate Project',
+    imgSrc: 'static/images/proj/nextjs-boilerplate.png',
+    href: 'https://template-next-two.vercel.app',
   },
 ]
 
