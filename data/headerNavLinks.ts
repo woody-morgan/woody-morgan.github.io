95c5aeb3258f0ec8347219c7b1d523478aa908a4
woodi97 <woodi.daily@gmail.com>
2022-03-26 12:49:08 +0900

    Remove: Resume Page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/resume', title: 'Resume' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
