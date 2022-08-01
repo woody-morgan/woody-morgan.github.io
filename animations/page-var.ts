641812cec90b957d9eed53717fdf85afa9e8bc05
woodi97 <woodi.daily@gmail.com>
2022-08-01 16:43:53 +0900

    publish(): making full page scroll effect

--- animations/page-var.ts
+++ animations/page-var.ts
@@ -1,7 +1,7 @@
 import { Variants } from 'framer-motion'
 
 export const PageVariants: Variants = {
-  hidden: { opacity: 0, x: -500, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 500, y: 0 },
+  hidden: { opacity: 0 },
+  enter: { opacity: 1 },
+  exit: { opacity: 0 },
 }
