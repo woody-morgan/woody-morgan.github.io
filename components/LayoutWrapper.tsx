da8012a8f624068e1d9daa4d030831cf3a1213a0
woody-morgan <woody.morgan.crafts@gmail.com>
2024-10-21 00:16:29 +0900

    chore: comback logo

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,5 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
+import LogoSvg from '@/data/Logo'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -19,6 +20,9 @@ const LayoutWrapper = ({ children }: Props) => {
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
+                <div className="mr-3">
+                  <LogoSvg />
+                </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
