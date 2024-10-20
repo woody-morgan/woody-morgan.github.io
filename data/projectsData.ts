975ba06dacc856d61d66be25c6174ca235a4eb3c
woody-morgan <woody.morgan.crafts@gmail.com>
2024-10-20 22:44:48 +0900

    chore: update project(add chakchak)

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,25 @@
 const projectsData = [
   {
+    title: 'Chak Chak',
+    description: '팝업스토어 예약 SaaS',
+    imgSrc: 'static/images/proj/chak-chak.png',
+    href: 'https://chak-chak.vercel.app',
+  },
+  {
     title: 'Surrounds',
-    description: 'Voice Chat SNS Based on Your Location',
+    description: '위치 기반 음성 채팅 SNS',
     imgSrc: 'static/images/proj/surrounds.png',
     href: 'https://surrounds-fe.vercel.app',
   },
   {
     title: 'Study Cafe',
-    description: 'Online Study Management Platform',
+    description: '온라인 스터디 카페 플랫폼',
     imgSrc: 'static/images/proj/study-cafe.png',
     href: 'https://study-cafe.vercel.app',
   },
   {
     title: 'With You',
-    description: 'Baby Logger Application',
+    description: '육아일기 앱',
     imgSrc: 'static/images/proj/with-you.png',
     href: 'https://with-you-front-end.vercel.app',
   },
